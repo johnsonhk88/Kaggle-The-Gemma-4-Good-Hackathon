b4208210f5bc73250fb23e437196d3c657e0c775
Johnson Chong <johnsonhk88@gmail.com>
2026-04-14 17:52:57 +0800

    feat : update support build local environment for gemma4 with GPU V100 and CUDA 12.8

---
+++ pytorch-install.sh
@@ -0,0 +1,5 @@
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+
+
+# Install the version that supports V100 but works with your 12.8 driver
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
