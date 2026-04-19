08e8fb15f75ba9147b34f3c0ed50c94f36b7de76
Johnson Chong <johnsonhk88@gmail.com>
2026-04-19 18:08:14 +0800

    feat : update local code support video analysis and live camera by opencv

--- pytorch-install.sh
+++ pytorch-install.sh
@@ -3,5 +3,9 @@ pip uninstall torch torchvision torchaudio -y
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 
 
+# install for window envirorment used at least driver
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu130
+
+
 # Install the version that supports V100 but works with your 12.8 driver
 # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
